9a83b2fead5a96604829f8a9961829c77e944c26
Yusuke Saito <ue-saitou@kddi.com>
2026-04-20 08:07:36 +0000

    prompt 1 + 2

---
+++ models/staging/stg_part_suppliers.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select * from {{ source('tpch', 'partsupp') }}
+
+),
+
+renamed as (
+
+    select
+        ps_partkey as part_key,
+        ps_suppkey as supplier_key,
+        ps_availqty as available_quantity,
+        ps_supplycost as supply_cost,
+        ps_comment as comment
+    from source
+
+)
+
+select * from renamed
